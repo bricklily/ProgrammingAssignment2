b289617168280dff55babf810ae82b14a19016e6
bricklily <steenparl@Steens-MBP.home>
2015-04-25 10:47:31 -0400

    initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## These function can cache a matrix inverse to avoid repeating an inverse.
 
 ## Write a short comment describing this function
+## This function returns a list of functions to 
+##	1. set a matrix
+##	2. get a matrix
+##	3. set the inverse matrix
+##	4. get the inverse of the matrix
+## Calling this with a square matrix will cache it in the returned 'cached matrix'
+makeCacheMatrix <- function(x = matrix()) 
+{
+	inverse <- NULL
+	## The set-function defined
+	set <- function(y)
+	{
+		## Only set if this is a square matrix
+		if( nrow(y)==ncol(y) )
+		{
+			x       <<- y
+			inverse <<- NULL
+		} else
+		{
+			print("Warning: Cannot set a non-square matrix")
+		}
+	}
+	
+	## The get-function defined
+	get <- function() x
 
-makeCacheMatrix <- function(x = matrix()) {
+	## The setinverse-function defined
+	setinverse <- function(inv) inverse <<- inv
 
+	## The getinverse-function defined
+	getinverse <- function() inverse
+	
+	## Create the returned list of functions
+	list( set = set
+		, get = get
+		, setinverse = setinverse
+		, getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## This function calculates the inverse of a 'cached matrix'
+cacheSolve <- function(x, ...) 
+{
+	inv <- x$getinverse()
+	if(!is.null(inv))
+	{
+		message("getting cached inverse")
+		return (inv)
+	}
+	mat <- x$get()
+	inv <- solve(mat)
+	x$setinverse(inv)
+    ## Return a matrix that is the inverse of 'x'
+	inv
 }
